5a1fb89372a93b140bb976411d85dc9042edd5c9
Divyanshu <divyanshusharma092@gmail.com>
2026-07-03 13:36:53 +0530

    Factorial in cpp

---
+++ discount.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int total_discount;
+    int amount;
+    int discount=0;
+    cin>>amount;
+    if(amount<1000){
+        total_discount=amount;
+    } 
+    else if(amount>=1000 && amount<5000){
+        discount=(amount*10)/100;
+        total_discount=amount-discount;
+       
+    }else if(amount>=5000)
+    {
+        discount=(amount*15)/100;
+        total_discount=amount-discount;
+        
+    }
+    cout<<total_discount<<endl;
+    return 0; 
+}
